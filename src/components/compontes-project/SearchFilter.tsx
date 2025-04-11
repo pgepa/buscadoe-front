09b2f9603a536546f64c41a20293953d39897c9c
Anderson Pontes <andersonpinheiro.developer@gmail.com>
2025-04-11 10:40:24 -0300

    alteração de layout

--- src/components/compontes-project/SearchFilter.tsx
+++ src/components/compontes-project/SearchFilter.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { Search, X } from 'lucide-react';
+import { Label } from '../ui/label';
 
 
 const SearchFilter: React.FC = () => {
@@ -51,50 +52,90 @@ const SearchFilter: React.FC = () => {
     };
 
     return (
-        <form className="flex flex-col sm:flex-row items-center gap-2 flex-wrap">
+        <>
+            <h3 className="text-base font-bold mb-2 text-blue-800/90">Filtros:</h3>
+    
+            <form className='grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-6 gap-2 mt-2'>
 
-            <span className="text-lg font-semibold text-blue-800/80">Pesquisar:</span>
-            <Input
-                placeholder="Informe o conteúdo a ser buscado"
-                value={localQuery.termo}
-                onChange={(e) => setLocalQuery({ ...localQuery, termo: e.target.value })}
-                className="w-full sm:w-[500px]"
-            />
-            <Input
-                placeholder="Ano"
-                value={localQuery.ano}
-                onChange={(e) => setLocalQuery({ ...localQuery, ano: e.target.value })}
-                className="w-full sm:w-auto"
-            />
-            <Input
-                placeholder="dd/mm/aaaa"
-                value={localQuery.data_inicio}
-                onChange={(e) => setLocalQuery({ ...localQuery, data_inicio: e.target.value })}
-                className="w-full sm:w-auto"
-            />
-            <Input
-                placeholder="dd/mm/aaaa"
-                value={localQuery.data_fim}
-                onChange={(e) => setLocalQuery({ ...localQuery, data_fim: e.target.value })}
-                className="w-full sm:w-auto"
-            />
+                <div className='space-y-2'>
+                    <Label className='font-semibold text-sm text-blue-800/80'>Busca:</Label>
+                    <div className="relative">
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                            <Search className="h-4 w-4 text-gray-500" />
+                        </span>
+                        <Input
+                            placeholder="Busca por termos"
+                            value={localQuery.termo}
+                            onChange={(e) => setLocalQuery({ ...localQuery, termo: e.target.value })}
+                            className='pl-10 col-span-1 sm:col-span-2 lg:col-span-3'
+                        />
+
+                    </div>
+                </div>
+
+                <div className='space-y-2'>
+                    <Label className='font-semibold text-sm text-blue-800/80'>Ano:</Label>
+                    <div className="relative">
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                            <Search className="h-4 w-4 text-gray-500" />
+                        </span>
+                        <Input
+                            placeholder="Ano"
+                            value={localQuery.ano}
+                            onChange={(e) => setLocalQuery({ ...localQuery, ano: e.target.value })}
+                            className='pl-10 col-span-1'
+                        />
+
+                    </div>
+                </div>
+
+                <div className='space-y-2'>
+                    <Label className='font-semibold text-sm text-blue-800/80'>Período inicial:</Label>
+                    <div className="relative">
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                            <Search className="h-4 w-4 text-gray-500" />
+                        </span>
+                        <Input
+                            placeholder="dd/mm/aaaa"
+                            value={localQuery.data_inicio}
+                            onChange={(e) => setLocalQuery({ ...localQuery, data_inicio: e.target.value })}
+                            className='pl-10 col-span-1'
+                        />
+
+                    </div>
+                </div>
+
+                <div className='space-y-2'>
+                    <Label className='font-semibold text-sm text-blue-800/80'>Período final:</Label>
+                    <div className="relative">
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                            <Search className="h-4 w-4 text-gray-500" />
+                        </span>
+                        <Input
+                            placeholder="dd/mm/aaaa"
+                            value={localQuery.data_fim}
+                            onChange={(e) => setLocalQuery({ ...localQuery, data_fim: e.target.value })}
+                            className='pl-10 col-span-1'
+                        />
+
+                    </div>
+                </div>
+
+                <Button onClick={handleSearch} type="submit" size="default" variant="default" className="mt-8 bg-blue-800/80 hover:bg-blue-800/90 text-white flex items-center">
+                    <Search className="h-4 w-4 mr-2" />
+                    Pesquisar
+                </Button>
+
+                <Button onClick={handleClearFilters} variant="outline" size="default" className="mt-8">
+                    <X className="h-4 w-4 mr-2" />
+                    Remover filtros
+                </Button>
 
 
-            <Button onClick={handleSearch} type="submit" size="default" variant="default" className="w-full sm:w-auto bg-blue-800/80 hover:bg-blue-800/90 text-white flex items-center">
-                <Search className="h-4 w-4 mr-2" />
-                Pesquisar
-            </Button>
 
-            <Button onClick={handleClearFilters} variant="outline" size="default" className="w-full sm:w-auto">
-                <X className="h-4 w-4 mr-2" />
-                Remover filtros
-            </Button>
+            </form>
 
-
-
-        </form>
-
-
+        </>
     );
 };
 
